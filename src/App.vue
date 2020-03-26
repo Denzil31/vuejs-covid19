07e9c5415767905d5c7c3d7bb73e4431f7240a78
= <=>
2020-03-26 19:42:36 +0530

    Added Table to represent JSON data

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="table-responsive">
+      <table class="table table-hover table-condensed">
+        <tr>
+          <th>Id</th>
+          <th>State</th>
+          <th>Indian</th>
+          <th>Foreign</th>
+          <th>Recovered</th>
+          <th>Dead</th>
+        </tr>
+        <tr v-for="value in msg.data" v-bind:key="value.id">
+          <th>{{ value.id }}</th>
+          <th>{{ value.state }}</th>
+          <th>{{ value.confirmIndian }}</th>
+          <th>{{ value.confirmForeign }}</th>
+          <th>{{ value.recovered }}</th>
+          <th>{{ value.death }}</th>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      msg: ""
+    };
+  },
+  methods: {
+    getMessage() {
+      const path = "https://covid-19-india-api.herokuapp.com/api";
+      axios
+        .get(path)
+        .then(res => {
+          this.msg = res.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  },
+  created() {
+    this.getMessage();
   }
-}
+};
 </script>
 
 <style>
@@ -21,7 +56,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
